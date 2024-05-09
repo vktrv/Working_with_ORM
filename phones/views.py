7060799b68bed447f503eb09c0e937bfc5158e0d
Olesya Viktorova <ole72sya2012@yandex.ru>
2024-05-09 14:49:36 +0500

    second commit

--- phones/views.py
+++ phones/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    if 'sort' in request.GET:
+        sort_type = str(request.GET.get('sort'))
+        print(sort_type)
+        if sort_type == 'min_price':
+            sort_type = 'price'
+            context = {
+                'phones': Phone.objects.order_by(sort_type)
+            }
+            return render(request, template, context)
+        if sort_type == 'max_price':
+            sort_type = 'price'
+            context = {
+                'phones': reversed(Phone.objects.order_by(sort_type))
+            }
+            return render(request, template, context)
+        context = {
+            'phones': Phone.objects.order_by(sort_type)
+        }
+        return render(request, template, context)
+    context = {
+        'phones': Phone.objects.all()
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
